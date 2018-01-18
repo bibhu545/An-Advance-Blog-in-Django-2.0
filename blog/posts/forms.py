b1c2cf6e0a68a23c4755a68ec21b9caf10d26cc4
Bibhu Ranjan <bibhuranjan.500@gmail.com>
2018-01-18 17:29:37 +0530

    Add files via upload
    Base Files

---
+++ blog/posts/forms.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm
+
+from .models import Post
+
+class PostForm(ModelForm):
+	class Meta():
+		model = Post
+		fields = [
+			"title",
+			"image",
+			"content",
+			"draft",
+			"publish"
+		]
+	def __init__(self, *args, **kwargs):
+		super(PostForm, self).__init__(*args,**kwargs)
+		self.fields['title'].widget.attrs.update({'class':'form-control'})
+		self.fields['title'].widget.attrs.update({'placeholder':'Enter Title'})
+		self.fields['image'].widget.attrs.update({'class':'form-control'})
+		self.fields['image'].widget.attrs.update({'placeholder':'Choose Image'})
+		self.fields['content'].widget.attrs.update({'class':'form-control'})
+		self.fields['content'].widget.attrs.update({'placeholder':'Enter Content Here'})
+# from django.forms import ModelForm
+# from posts.models import Post
+# class PostForm(ModelForm):
+# 	class Meta:
+# 		model = Post
+# 		fields = ['title','content']
